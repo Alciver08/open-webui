49be7e171d38207762ce786aa9b80ea29304388f
Alciver08 <alciver08@gmail.com>
2024-10-13 16:42:41 -0500

    .

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -147,6 +147,14 @@ def parse_section(section):
     return items
 
 
+# try:
+#     changelog_path = BASE_DIR / "CHANGELOG.md"
+#     with open(str(changelog_path.absolute()), "r", encoding="utf8") as file:
+#         changelog_content = file.read()
+
+# except Exception:
+#     changelog_content = (pkgutil.get_data("open_webui", "CHANGELOG.md") or b"").decode()
+
 try:
     changelog_content = (pkgutil.get_data("open_webui", "CHANGELOG.md") or b"").decode()
 except FileNotFoundError:
